3faec7033699e3d4845be8c502f298cee2df46c8
stocktons <sarah@thanksforallthe.fish>
2022-04-13 10:12:28 -0400

    streamlined dropdowns

--- src/Form.js
+++ src/Form.js
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import './Form.css';
 
 export default function Form({ updateMessage }) {
-    const INITIAL_STATE = { first: "a", middle: "none", last: "a", month: "January", date: "1" };
-    const [formData, setFormData] = useState(INITIAL_STATE)
+    const INITIAL_STATE = { first: "--", middle: "none", last: "a", month: "January", date: "1" };
+    const [formData, setFormData] = useState(INITIAL_STATE);
+    const alphabet = Array.from('abcdefghijklmnopqrstuvwxyz');
+    const months = Array.from({length: 12}, (item, i) => {
+        return new Date(0, i).toLocaleString('en-US', {month: 'long'})
+      });
+    const days = [...Array(32).keys()].slice(1);
 
     function handleChange(evt) {
         const { name, value } = evt.target;
@@ -25,152 +30,41 @@ export default function Form({ updateMessage }) {
                 First Initial
             </label>
             <select className="dropdown" id="first" value={formData.first} name="first" onChange={handleChange}>
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
-                <option value="e">E</option>
-                <option value="f">F</option>
-                <option value="g">G</option>
-                <option value="h">H</option>
-                <option value="i">I</option>
-                <option value="j">J</option>
-                <option value="k">K</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-                <option value="n">N</option>
-                <option value="o">O</option>
-                <option value="p">P</option>
-                <option value="q">Q</option>
-                <option value="r">R</option>
-                <option value="s">S</option>
-                <option value="t">T</option>
-                <option value="u">U</option>
-                <option value="v">V</option>
-                <option value="w">W</option>
-                <option value="x">X</option>
-                <option value="y">Y</option>
-                <option value="z">Z</option>
+                {/* <option disabled>--</option> */}
+                {alphabet.map((a) => <option value={a} key={a}>{a.toUpperCase()}</option>)}
             </select>
 
             <label id="middle_label">
                 Middle Initial
             </label>
             <select className="dropdown" id="middle" value={formData.middle} name="middle" onChange={handleChange}>
+                {/* <option disabled>--</option> */}
                 <option value="none">none</option>
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
-                <option value="e">E</option>
-                <option value="f">F</option>
-                <option value="g">G</option>
-                <option value="h">H</option>
-                <option value="i">I</option>
-                <option value="j">J</option>
-                <option value="k">K</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-                <option value="n">N</option>
-                <option value="o">O</option>
-                <option value="p">P</option>
-                <option value="q">Q</option>
-                <option value="r">R</option>
-                <option value="s">S</option>
-                <option value="t">T</option>
-                <option value="u">U</option>
-                <option value="v">V</option>
-                <option value="w">W</option>
-                <option value="x">X</option>
-                <option value="y">Y</option>
-                <option value="z">Z</option>
+                {alphabet.map((a) => <option value={a} key={a}>{a.toUpperCase()}</option>)}
             </select>
 
             <label id="last_label">
                 Last Initial
             </label>
             <select className="dropdown" id="last" value={formData.last} name="last" onChange={handleChange}>
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
-                <option value="e">E</option>
-                <option value="f">F</option>
-                <option value="g">G</option>
-                <option value="h">H</option>
-                <option value="i">I</option>
-                <option value="j">J</option>
-                <option value="k">K</option>
-                <option value="l">L</option>
-                <option value="m">M</option>
-                <option value="n">N</option>
-                <option value="o">O</option>
-                <option value="p">P</option>
-                <option value="q">Q</option>
-                <option value="r">R</option>
-                <option value="s">S</option>
-                <option value="t">T</option>
-                <option value="u">U</option>
-                <option value="v">V</option>
-                <option value="w">W</option>
-                <option value="x">X</option>
-                <option value="y">Y</option>
-                <option value="z">Z</option>
+                {/* <option disabled>--</option> */}
+                {alphabet.map((a) => <option value={a} key={a}>{a.toUpperCase()}</option>)}
             </select>
 
             <label id="month_label">
                 Birth Month
             </label>
             <select className="dropdown" id="month" value={formData.month} name="month" onChange={handleChange}>
-                <option value="January">January</option>
-                <option value="February">February</option>
-                <option value="March">March</option>
-                <option value="April">April</option>
-                <option value="May">May</option>
-                <option value="June">June</option>
-                <option value="July">July</option>
-                <option value="August">August</option>
-                <option value="September">September</option>
-                <option value="October">October</option>
-                <option value="November">November</option>
-                <option value="December">December</option>
+                {/* <option disabled>--</option> */}
+                {months.map((m) => <option value={m} key={m}>{m}</option>)}
             </select>
 
             <label id="day_label">
                 Birth Date
             </label>
             <select className="dropdown" id="date" value={formData.date} name="date" onChange={handleChange}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
+                {/* <option disabled>--</option> */}
+                {days.map((a) => <option value={a} key={a.toString()}>{a}</option>)}
             </select>
 
             <input id="submit-button" type="submit" value="Show me my new job!" />
